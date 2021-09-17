b19ce19a547b0c60e31779892e973d5496e5633d
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-09-16 19:42:45 -0500

    Make formatting a little more F#like

--- src/Swashbuckle.NodaTime.AspNetCore/Schemas.fs
+++ src/Swashbuckle.NodaTime.AspNetCore/Schemas.fs
@@ -8,21 +8,20 @@ open NodaTime
 open NodaTime.TimeZones
 
 module internal Schemas =
-  type Container =
-    {Instant : OpenApiSchema;
-     LocalDate : OpenApiSchema;
-     LocalTime : OpenApiSchema;
-     LocalDateTime : OpenApiSchema;
-     OffsetDateTime : OpenApiSchema;
-     ZonedDateTime : OpenApiSchema;
-     Interval : OpenApiSchema;
-     Offset : OpenApiSchema;
-     Period : OpenApiSchema;
-     Duration : OpenApiSchema;
-     DateTimeZone : OpenApiSchema}
+  type Container = {
+    Instant : OpenApiSchema;
+    LocalDate : OpenApiSchema;
+    LocalTime : OpenApiSchema;
+    LocalDateTime : OpenApiSchema;
+    OffsetDateTime : OpenApiSchema;
+    ZonedDateTime : OpenApiSchema;
+    Interval : OpenApiSchema;
+    Offset : OpenApiSchema;
+    Period : OpenApiSchema;
+    Duration : OpenApiSchema;
+    DateTimeZone : OpenApiSchema }
 
   type SchemaCreator(?serializerSettings : JsonSerializerSettings) =
-
     let defaultSerializer =
       match JsonConvert.DefaultSettings with
       | null -> null
@@ -34,10 +33,10 @@ module internal Schemas =
     // So made a type to have a private method declared
     member private __.StringSchema(value, ?format) =
       OpenApiSchema(Type = "string",
-             Example = OpenApiString(JsonConvert.SerializeObject(value, settings) |> JsonConvert.DeserializeObject<string>),
-             Format = match format with
-                      | Some x -> x
-                      | None -> null)
+        Example = OpenApiString(JsonConvert.SerializeObject(value, settings) |> JsonConvert.DeserializeObject<string>),
+        Format = match format with
+                 | Some x -> x
+                 | None -> null)
 
     member __.Create() =
       let dateTimeZone =
@@ -45,38 +44,27 @@ module internal Schemas =
           DateTimeZoneProviders.Tzdb.GetSystemDefault()
         with :? DateTimeZoneNotFoundException ->
           DateTimeZoneProviders.Tzdb.["America/New_York"]
-
       let instant = Instant.FromUnixTimeTicks(15759724104663180L)
       let zonedDateTime = instant.InZone dateTimeZone
-      let interval =
-        Interval
-          (instant,
-           instant.PlusTicks(TimeSpan.TicksPerDay)
-                  .PlusTicks(TimeSpan.TicksPerHour)
-                  .PlusTicks(TimeSpan.TicksPerMinute)
-                  .PlusTicks(TimeSpan.TicksPerSecond)
-                  .PlusTicks(TimeSpan.TicksPerMillisecond))
-      {Container.Instant = __.StringSchema(instant, "date-time");
-       Container.LocalDate = __.StringSchema(zonedDateTime.Date, "full-date");
-       Container.LocalTime =
-         __.StringSchema(zonedDateTime.TimeOfDay, "partial-time");
-       Container.LocalDateTime = __.StringSchema zonedDateTime.LocalDateTime;
-       Container.OffsetDateTime =
-         __.StringSchema(instant.WithOffset zonedDateTime.Offset, "date-time");
-       Container.ZonedDateTime = __.StringSchema zonedDateTime;
-       Container.Interval =
-         OpenApiSchema
-           (Type = "object",
-            Properties = dict
-                           [("Start", __.StringSchema(interval.Start, "date-time"));
-                            ("End", __.StringSchema(interval.End, "date-time"))]);
-       Container.Offset =
-         __.StringSchema(zonedDateTime.Offset, "time-numoffset");
-       Container.Period =
-         __.StringSchema
-           (Period.Between
-              (zonedDateTime.LocalDateTime,
-               interval.End.InZone(dateTimeZone).LocalDateTime,
-               PeriodUnits.AllUnits));
-       Container.Duration = __.StringSchema interval.Duration;
-       Container.DateTimeZone = __.StringSchema dateTimeZone}
+      let interval = Interval(instant, instant
+        .PlusTicks(TimeSpan.TicksPerDay)
+        .PlusTicks(TimeSpan.TicksPerHour)
+        .PlusTicks(TimeSpan.TicksPerMinute)
+        .PlusTicks(TimeSpan.TicksPerSecond)
+        .PlusTicks(TimeSpan.TicksPerMillisecond))
+      { Container.Instant = __.StringSchema(instant, "date-time");
+        Container.LocalDate = __.StringSchema(zonedDateTime.Date, "full-date");
+        Container.LocalTime = __.StringSchema(zonedDateTime.TimeOfDay, "partial-time");
+        Container.LocalDateTime = __.StringSchema zonedDateTime.LocalDateTime;
+        Container.OffsetDateTime = __.StringSchema(instant.WithOffset zonedDateTime.Offset, "date-time");
+        Container.ZonedDateTime = __.StringSchema zonedDateTime;
+        Container.Interval = OpenApiSchema(Type = "object", Properties = dict
+          [("Start", __.StringSchema(interval.Start, "date-time"));
+          ("End", __.StringSchema(interval.End, "date-time"))]);
+        Container.Offset = __.StringSchema(zonedDateTime.Offset, "time-numoffset");
+        Container.Period = __.StringSchema(Period.Between
+          (zonedDateTime.LocalDateTime,
+          interval.End.InZone(dateTimeZone).LocalDateTime,
+          PeriodUnits.AllUnits));
+        Container.Duration = __.StringSchema interval.Duration;
+        Container.DateTimeZone = __.StringSchema dateTimeZone }
